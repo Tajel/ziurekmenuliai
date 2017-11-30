3ef5748cb750a26cd36d0e544796db6311f5098f
Tomas Kulikauskas <tajel111@gmail.com>
2017-11-30 14:58:03 +0200

    DV edit 1st try

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
 var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    flash = require("connect-flash"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    User = require("./models/user"),
-    expressSanitizer = require("express-sanitizer");
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  User = require("./models/user"),
+  expressSanitizer = require("express-sanitizer");
+
 //requiring routes
-var
-    autokrovRoutes = require("./routes/autokrov"),
-    autolenRoutes = require("./routes/autolen"),
-    automechRoutes = require("./routes/automech"),
-    autommRoutes = require("./routes/automm"),
-    autonuomaRoutes = require("./routes/autonuoma"),
-    autoikRoutes = require("./routes/autoik"),
-    vairRoutes = require("./routes/vair"),
-    vairikRoutes = require("./routes/vairik"),
-    dvRoutes = require("./routes/dv"),
-    objRoutes = require("./routes/obj"),
-    padRoutes = require("./routes/pad"),
-    trrusRoutes = require("./routes/trrus"),
-    trtipRoutes = require("./routes/trtip"),
-    kurrusRoutes = require("./routes/kurrus"),
-    darbrusRoutes = require("./routes/darbrus"),
-    kuruzpRoutes = require("./routes/kuruzp"),
-    imonesRoutes = require("./routes/imones"),
-    kellapRoutes = require("./routes/kellap"),
-    kellapajamosRoutes = require("./routes/kellapajamos"),
-    kellapkurasRoutes = require("./routes/kellapkuras"),
-    kellapvairRoutes = require("./routes/kellapvair"),
-    testRoutes = require("./routes/test"),
-    indexRoutes = require("./routes/index");
+var autokrovRoutes = require("./routes/autokrov"),
+  autolenRoutes = require("./routes/autolen"),
+  automechRoutes = require("./routes/automech"),
+  autommRoutes = require("./routes/automm"),
+  autonuomaRoutes = require("./routes/autonuoma"),
+  autoikRoutes = require("./routes/autoik"),
+  vairRoutes = require("./routes/vair"),
+  vairikRoutes = require("./routes/vairik"),
+  dvRoutes = require("./routes/dv"),
+  objRoutes = require("./routes/obj"),
+  padRoutes = require("./routes/pad"),
+  trrusRoutes = require("./routes/trrus"),
+  trtipRoutes = require("./routes/trtip"),
+  kurrusRoutes = require("./routes/kurrus"),
+  darbrusRoutes = require("./routes/darbrus"),
+  kuruzpRoutes = require("./routes/kuruzp"),
+  imonesRoutes = require("./routes/imones"),
+  kellapRoutes = require("./routes/kellap"),
+  kellapajamosRoutes = require("./routes/kellapajamos"),
+  kellapkurasRoutes = require("./routes/kellapkuras"),
+  kellapvairRoutes = require("./routes/kellapvair"),
+  testRoutes = require("./routes/test"),
+  indexRoutes = require("./routes/index");
 
 var url = process.env.DATABASEURL || "mongodb://localhost/transport2";
 mongoose.connect(url, {
-    useMongoClient: true
+  useMongoClient: true
 });
 mongoose.Promise = global.Promise;
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(expressSanitizer());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -51,22 +53,24 @@ app.use(flash());
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function (req, res, next) {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
@@ -103,10 +107,9 @@ app.use("/kellap/:id/vair", kellapvairRoutes);
 //   res.send('@@@@ LOG @@@@@')
 // })
 
-
 // app.listen(process.env.PORT, process.env.IP, function(){
 //    console.log("TRANSPORT Server Has Started!");
 
-app.listen(3000, "127.0.0.1", function () {
-    console.log("TRANSOPORTO MODULIS server started port:3000");
+app.listen(3000, "127.0.0.1", function() {
+  console.log("TRANSOPORTO MODULIS server started port:3000");
 });
