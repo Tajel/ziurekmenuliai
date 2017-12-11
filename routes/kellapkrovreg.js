098e6c4894dacf24ead590f00f4a1f46cd517bcf
Tomas Kulikauskas <tajel111@gmail.com>
2017-12-11 15:45:49 +0200

    kellapkrovreg preparations

---
+++ routes/kellapkrovreg.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+var async = require("async");
+// var Auto = require("../models/auto");
+var Kellap = require("../models/kellap");
+var Autokellap = require("../models/autokellap");
+var middleware = require("../middleware");
+
+module.exports = router;
