cf279c6a483050f929190990f9aac7d6df025d29
Tomas Kulikauskas <tajel111@gmail.com>
2017-12-14 15:53:58 +0200

    kellap detail styling

--- public/scripts/kellapfilter.js
+++ public/scripts/kellapfilter.js
@@ -1,130 +1,134 @@
 function myFunction1() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput1");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput1");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[0];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
     }
+  }
 }
-
-
 function myFunction2() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput2");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[4];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput2");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[3];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
     }
+  }
 }
-
 
 function myFunction3() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput3");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[7];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput3");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[6];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
     }
+  }
 }
-
 
 function myFunction4() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput4");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[8];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput4");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[7];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
     }
+  }
 }
-
 
 function myFunction5() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput5");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[6];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput5");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[5];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
     }
+  }
 }
-
 
 function myFunction6() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput6");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[5];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput6");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[4];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
     }
+  }
+}
+function myFunction7() {
+  var input, filter, row, data, i;
+  input = document.getElementById("myInput7");
+  filter = input.value.toUpperCase();
+  row = document.getElementsByClassName("kellap-data__row");
+  for (i = 0; i < row.length; i++) {
+    data = row[i].getElementsByTagName("div")[4];
+    if (data) {
+      if (data.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
+    }
+  }
 }
 
-function myFunction7() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput7");
-    filter = input.checked;
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("input")[0];
-        if (td) {
-            if (td.checked === filter) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
+// function myFunction7() {
+//   var input, filter, table, tr, td, i;
+//   input = document.getElementById("myInput7");
+//   filter = input.checked;
+//   table = document.getElementById("myTable");
+//   tr = table.getElementsByTagName("tr");
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("input")[0];
+//     if (td) {
+//       if (td.checked === filter) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }
+//   }
+// }
